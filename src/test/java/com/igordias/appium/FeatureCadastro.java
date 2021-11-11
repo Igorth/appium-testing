15a1c35082afb00c93196349ca120874b90752f0
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-11-11 15:49:25 -0400

    first test, clicking on sign up

--- src/test/java/com/igordias/appium/FeatureCadastro.java
+++ src/test/java/com/igordias/appium/FeatureCadastro.java
@@ -1,4 +1,24 @@
 package com.igordias.appium;
 
+import io.appium.java_client.MobileElement;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FeatureCadastro {
+
+    @Test
+    @DisplayName("When the user click on Cadastrar field, then show me the page Cadastrar")
+    public void can_not_register_users_with_passwords_that_do_not_match() throws MalformedURLException {
+        AppiumDriverConfig driver = new AppiumDriverConfig();
+
+        MobileElement botaoCadastro = (MobileElement)driver.driver.findElementById(
+                "br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+        botaoCadastro.click();
+
+        assertTrue(true);
+    }
 }
