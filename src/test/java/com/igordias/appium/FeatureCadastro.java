c44c30dca84a3d7ae69fccf95eb18dade7f954db
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-11-11 16:24:24 -0400

    added a second test, transition between pages

--- src/test/java/com/igordias/appium/FeatureCadastro.java
+++ src/test/java/com/igordias/appium/FeatureCadastro.java
@@ -3,6 +3,7 @@ package com.igordias.appium;
 import io.appium.java_client.MobileElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
 
 import java.net.MalformedURLException;
 
@@ -34,5 +35,28 @@ public class FeatureCadastro {
         MobileElement erro = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
 
         assertEquals("Senhas não conferem", erro.getText());
+    }
+
+    @Test
+    @DisplayName("When the user try to sign up with equals passwords, then the user is redirect to the home page")
+    public void register_users_with_matching_passwords() throws NoSuchElementException, MalformedURLException {
+        AppiumDriverConfig driver = new AppiumDriverConfig();
+
+        MobileElement botaoCadastro = (MobileElement)driver.driver.findElementById(
+                "br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+        botaoCadastro.click();
+
+        MobileElement campoNome = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+        MobileElement campoSenha = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+        MobileElement campoConfirmarSenha = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+        campoNome.setValue("Igor");
+        campoSenha.setValue("123");
+        campoConfirmarSenha.setValue("123");
+
+        MobileElement botaoConfirmarCadastro = (MobileElement)driver.driver.findElementById(
+                "br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+        botaoConfirmarCadastro.click();
+
+        MobileElement botaoLogar = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/login_botao_logar");
     }
 }
