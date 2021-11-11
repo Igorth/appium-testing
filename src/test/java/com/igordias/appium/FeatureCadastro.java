9c72d3e21af891571ce9711cdaf0980248a162eb
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-11-11 16:01:12 -0400

    change display name test

--- src/test/java/com/igordias/appium/FeatureCadastro.java
+++ src/test/java/com/igordias/appium/FeatureCadastro.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class FeatureCadastro {
 
     @Test
-    @DisplayName("When the user click on Cadastrar field, then show me the page Cadastrar")
+    @DisplayName("When the user try to sign up with different passwords, then show me a error message 'Senhas não conferem'")
     public void can_not_register_users_with_passwords_that_do_not_match() throws MalformedURLException {
         AppiumDriverConfig driver = new AppiumDriverConfig();
 
@@ -19,6 +20,19 @@ public class FeatureCadastro {
                 "br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
         botaoCadastro.click();
 
-        assertTrue(true);
+        MobileElement campoNome = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+        MobileElement campoSenha = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+        MobileElement campoConfirmarSenha = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+        campoNome.setValue("Igor");
+        campoSenha.setValue("123");
+        campoConfirmarSenha.setValue("456");
+
+        MobileElement botaoConfirmarCadastro = (MobileElement)driver.driver.findElementById(
+                "br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+        botaoConfirmarCadastro.click();
+
+        MobileElement erro = (MobileElement)driver.driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
+
+        assertEquals("Senhas não conferem", erro.getText());
     }
 }
